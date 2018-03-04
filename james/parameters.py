0170b5bd9868bc32ee1b9461df833818ab4c309e
James Arambam <jamesastrick@gmail.com>
2018-03-05 00:49:05 +0800

    First commit

---
+++ james/parameters.py
@@ -0,0 +1,13 @@
+
+
+
+# -------- Ranking Function -------- #
+U_DIM = 5
+I1_DIM = 5
+I2_DIM = 5
+HIDDEN_DIM = 10
+INPUT_DIM = U_DIM + I1_DIM + I2_DIM
+OUTPUT_DIM = 2 * 4
+
+LEARNING_RATE = 1e-3
+SEED = 1
