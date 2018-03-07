46e42cf4a2a0f55ccbfb1e46e799b3693e6c996f
James Arambam <jamesastrick@gmail.com>
2018-03-07 23:14:18 +0800

    v070318 -

--- james/data.py
+++ james/data.py
@@ -30,30 +30,52 @@ torch.manual_seed(SEED)
 class syntheticData:
 
     def __init__(self):
-        self.userEmbMat = Variable(torch.randn(USER_EMB_DIM, TOTAL_USERS), requires_grad=False)
-        self.itemEmbMat = Variable(torch.randn(ITEM_EMB_DIM, TOTAL_ITEMS), requires_grad=False)
+        self.userEmbMat = np.random.rand(USER_EMB_DIM, TOTAL_USERS)
+        self.itemEmbMat = np.random.rand(ITEM_EMB_DIM, TOTAL_ITEMS)
         self.trainData, self.testData = self.prepData()
 
     def prepData(self):
 
         self.binaryHash = {(0, 0):0, (0, 1):1, (1, 0):2, (1, 1):3}
-        arr = np.random.randint(0, 2, size=(TOTAL_USERS, TOTAL_ITEMS))
-        self.RatingMat = torch.from_numpy(arr)
+        self.RatingMat = np.random.randint(0, 2, size=(TOTAL_USERS, TOTAL_ITEMS))
         tmpTrain = []
         tmpTest = []
         for u in range(TOTAL_USERS):
             for i1 in range(TOTAL_ITEMS):
                 for i2 in range(i1+1, TOTAL_ITEMS):
                     tmp2 = []
-                    tmp2.append(u)
-                    tmp2.append(i1)
-                    tmp2.append(i2)
+                    tmp2.append(list(self.userEmbMat[:,u]))
+                    tmp2.append(list(self.itemEmbMat[:,i1]))
+                    tmp2.append(list(self.itemEmbMat[:,i2]))
+
                     cls = (self.RatingMat[u][i1], self.RatingMat[u][i2])
                     tmp2.append(self.binaryHash[cls])
                     if self.binaryHash[cls] == 0:
                         tmpTest.append(tmp2)
                     else:
                         tmpTrain.append(tmp2)
+                    tmp3  = np.array(tmp2)
+                    print tmp3.shape
+                    print tmp3
+                    exit()
+
+
+        for x in tmpTrain:
+            print x
+            print self.userEmbMat[:,x[0]]
+
+            exit()
+
+
+
+        exit()
+
+
+        print tmpTrain[0][0]
+        print tmpTrain[0][1]
+        print tmpTrain[0][2]
+        print tmpTrain[0][3]
+        exit()
 
         return torch.FloatTensor(tmpTrain), torch.FloatTensor(tmpTest)
 
