ca7c18ecd90b5dd9f063b9ab05c95ca3b1342662
James Arambam <jamesastrick@gmail.com>
2018-03-20 15:57:32 +0800

    v200318 -

--- james/rankingFn.py
+++ james/rankingFn.py
@@ -31,6 +31,7 @@ class Network(torch.nn.Module):
     self.linear1 = torch.nn.Linear(D_in, H)
     self.linear2 = torch.nn.Linear(H, D_out)
 
+
   def forward(self, x):
     h_relu = self.linear1(x).clamp(min=0)
     y_pred = self.linear2(h_relu)
